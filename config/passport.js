3f2b1810497d2445282a453f808e3e7b3e136093
Ashikur Rahman Nabir <ashike.cse.bd@gmail.com>
2018-12-16 18:53:33 +0600

    connect multiple link to an account

--- config/passport.js
+++ config/passport.js
@@ -19,7 +19,6 @@ module.exports = function (passport) {
 		});
 	});
 
-
 	passport.use('local-signup', new LocalStrategy({
 		usernameField: 'email',
 		passwordField: 'password',
@@ -32,7 +31,8 @@ module.exports = function (passport) {
 						return done(err);
 					if (user) {
 						return done(null, false, req.flash('signupMessage', 'That email already taken'));
-					} else {
+					}
+					if (!req.user) {
 						var newUser = new User();
 						newUser.local.email = email || "";
 						newUser.local.password = newUser.generateHash(password) || "";
@@ -41,9 +41,20 @@ module.exports = function (passport) {
 							if (err)
 								throw err;
 							return done(null, newUser);
-						})
+						});
 					}
-				})
+					else {
+						var user = req.user;
+						user.local.email = email || "";
+						user.local.password = user.generateHash(password) || "";
+
+						user.save(function (err) {
+							if (err)
+								throw err;
+							return done(null, user);
+						});
+					}
+				});
 
 			});
 		}));
@@ -74,67 +85,102 @@ module.exports = function (passport) {
 		clientID: configAuth.facebookAuth.clientId,
 		clientSecret: configAuth.facebookAuth.clientSecret,
 		callbackURL: configAuth.facebookAuth.callbackURL,
-		profileFields: ['id', 'displayName', 'email', 'birthday', 'friends', 'first_name', 'last_name', 'middle_name', 'gender', 'link']
-	  },
-	  function(accessToken, refreshToken, profile, done) {
-		process.nextTick(function (){
-			User.findOne({'facebook.id': profile.id}, function(err, user){
-				if(err){
-					return done(err);
-				}
-				if(user){
-					return done(null, user);
-				} else{
-					var newUser = new User();
-					newUser.facebook.id = profile.id;
-					newUser.facebook.token = accessToken;
-					newUser.facebook.name = profile.displayName;
-					newUser.facebook.email = profile.emails[0].value;
+		profileFields: ['id', 'displayName', 'email', 'birthday', 'friends', 'first_name', 'last_name', 'middle_name', 'gender', 'link'],
+		passReqToCallback: true
+	},
+		function (req, accessToken, refreshToken, profile, done) {
+			process.nextTick(function () {
+				if (!req.user) {
+					User.findOne({ 'facebook.id': profile.id }, function (err, user) {
+						if (err) {
+							return done(err);
+						}
+						if (user) {
+							return done(null, user);
+						} else {
+							var newUser = new User();
+							newUser.facebook.id = profile.id;
+							newUser.facebook.token = accessToken;
+							newUser.facebook.name = profile.displayName;
+							newUser.facebook.email = profile.emails[0].value;
 
-					newUser.save(function(err){
-						if(err){
+							newUser.save(function (err) {
+								if (err) {
+									return err;
+								}
+								return done(null, newUser);
+							});
+						}
+
+					});
+				} else {
+					var user = req.user;
+					user.facebook.id = profile.id;
+					user.facebook.token = accessToken;
+					user.facebook.name = profile.displayName;
+					user.facebook.email = profile.emails[0].value;
+
+					user.save(function (err) {
+						if (err) {
 							return err;
 						}
-						return done(null, newUser);
+						return done(null, user);
 					});
 				}
 
 			});
-		});
-	  }
+		}
 	));
 	passport.use(new GoogleStrategy({
 		clientID: configAuth.googleAuth.clientId,
 		clientSecret: configAuth.googleAuth.clientSecret,
-		callbackURL: configAuth.googleAuth.callbackURL
-	  },
-	  function(accessToken, refreshToken, profile, done) {
-		process.nextTick(function (){
-			User.findOne({'google.id': profile.id}, function(err, user){
-				if(err){
-					return done(err);
-				}
-				if(user){
-					return done(null, user);
-				} else{
-					var newUser = new User();
-					newUser.google.id = profile.id;
-					newUser.google.token = accessToken;
-					newUser.google.name = profile.displayName;
-					newUser.google.email = profile.emails[0].value;
+		callbackURL: configAuth.googleAuth.callbackURL,
+		passReqToCallback: true
+	},
+		function (req, accessToken, refreshToken, profile, done) {
+			process.nextTick(function () {
+				if (!req.user) {
+					User.findOne({ 'google.id': profile.id }, function (err, user) {
+						if (err) {
+							return done(err);
+						}
+						if (user) {
+							return done(null, user);
+						} else {
+							var newUser = new User();
+							newUser.google.id = profile.id;
+							newUser.google.token = accessToken;
+							newUser.google.name = profile.displayName;
+							newUser.google.email = profile.emails[0].value;
 
-					newUser.save(function(err){
-						if(err){
+							newUser.save(function (err) {
+								if (err) {
+									return err;
+								}
+								return done(null, newUser);
+							});
+							// console.log(profile);
+						}
+
+					});
+				} else {
+					var user = new User();
+					user.google.id = profile.id;
+					user.google.token = accessToken;
+					user.google.name = profile.displayName;
+					user.google.email = profile.emails[0].value;
+
+					user.save(function (err) {
+						if (err) {
 							return err;
 						}
-						return done(null, newUser);
+						return done(null, user);
 					});
-					console.log(profile);
+					// console.log(profile);
 				}
 
 			});
-		});
-	  }
+		}
 	));
 
 }
